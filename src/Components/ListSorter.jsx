0b986e60b0caad015bcff29f283e4f89aff3aa2c
Harry Keyes <harryakeyes7@gmail.com>
2020-02-05 11:05:42 +0000

    fix checkboxes to uncheck when cleared

--- src/Components/ListSorter.jsx
+++ src/Components/ListSorter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
   setOrder,
@@ -7,73 +7,96 @@ import {
   clearFilters
 } from "../redux/actions";
 
-let ListSorter = ({ dispatch }) => {
-  const handleChange = e => {
+class ListSorter extends Component {
+  state = {
+    gym: false,
+    carPark: false,
+    pool: false,
+    restaurant: false
+  };
+
+  handleChange = e => {
+    const { dispatch } = this.props;
     if (e.target.checked) {
+      this.setState({ [e.target.value]: true });
       dispatch(addFilter(e.target.name));
     } else {
+      this.setState({ [e.target.value]: false });
       dispatch(removeFilter(e.target.name));
     }
   };
 
-  const handleClick = () => {
+  handleClick = () => {
+    const { dispatch } = this.props;
+    this.setState({
+      gym: false,
+      pool: false,
+      restaurant: false,
+      carPark: false
+    });
     dispatch(clearFilters());
   };
-
-  return (
-    <div>
-      <label>
-        Sort by:{" "}
-        <select
-          onChange={e => {
-            e.preventDefault();
-            dispatch(setOrder(e.target.value));
-          }}
-        >
-          <option value={"desc"}>Highest Rated</option>
-          <option value={"asc"}>Lowest Rated</option>
-        </select>
-      </label>
-      <label>
-        Gym
-        <input
-          type="checkbox"
-          name="gym"
-          defaultChecked={false}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Car Park
-        <input
-          type="checkbox"
-          name="car park"
-          defaultChecked={false}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Pool
-        <input
-          type="checkbox"
-          name="pool"
-          defaultChecked={false}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Restaurant
-        <input
-          type="checkbox"
-          name="restaurant"
-          defaultChecked={false}
-          onChange={handleChange}
-        />
-      </label>
-      <button onClick={handleClick}>Clear Filters</button>
-    </div>
-  );
-};
+  render() {
+    const { dispatch } = this.props;
+    return (
+      <div>
+        <label>
+          Sort by:{" "}
+          <select
+            onChange={e => {
+              e.preventDefault();
+              dispatch(setOrder(e.target.value));
+            }}
+          >
+            <option value={"desc"}>Highest Rated</option>
+            <option value={"asc"}>Lowest Rated</option>
+          </select>
+        </label>
+        <label>
+          Gym
+          <input
+            type="checkbox"
+            name="gym"
+            checked={this.state.gym}
+            onChange={this.handleChange}
+            value="gym"
+          />
+        </label>
+        <label>
+          Car Park
+          <input
+            type="checkbox"
+            name="car park"
+            checked={this.state.carPark}
+            onChange={this.handleChange}
+            value="carPark"
+          />
+        </label>
+        <label>
+          Pool
+          <input
+            type="checkbox"
+            name="pool"
+            checked={this.state.pool}
+            onChange={this.handleChange}
+            value="pool"
+          />
+        </label>
+        <label>
+          Restaurant
+          <input
+            type="checkbox"
+            name="restaurant"
+            checked={this.state.restaurant}
+            onChange={this.handleChange}
+            value="restaurant"
+          />
+        </label>
+        <button onClick={this.handleClick}>Clear Filters</button>
+      </div>
+    );
+  }
+}
 
 ListSorter = connect()(ListSorter);
 
